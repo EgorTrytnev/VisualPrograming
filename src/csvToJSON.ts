7cdd1a3b4adabae9d4cce06ba94072be3f52d56a
EgorTrytnev <tryegor41@gmail.com>
2026-03-10 11:21:01 +0700

    All add

---
+++ src/csvToJSON.ts
@@ -0,0 +1,31 @@
+export function csvToJSON(input: string[], delimiter: string) : object[] {
+    if(!input || input.length < 2) {
+        throw new Error("Некорректная передача");
+    }
+
+    const result: object[] = [];
+
+    const header = input[0].split(delimiter);
+
+    for(let i = 1; i < input.length; i++) {
+        const values = input[i].split(delimiter);
+
+        if(header.length !== values.length) {
+            throw new Error("Несовпадение по количеству параметров");
+        }
+
+        const obj: { [key: string]: string | number } = {};
+        
+        for (let j = 0; j < header.length; j++) {
+            if(values[j] === "") {
+                throw new Error("Передача пустого значени"); 
+            }
+            
+            obj[header[j]] = isNaN(Number(values[j])) ? values[j] : Number(values[j]);
+        }
+
+        result.push(obj);
+    }
+
+    return result;
+}
